983c7f310fddc5c9bfaa801aed634077258169ff
serbandemosoiu <61535835+serbandemosoiu@users.noreply.github.com>
2023-04-04 14:16:54 +0300

    Update Backend.js

--- Backend.js
+++ Backend.js
@@ -1,43 +1,17 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-
 const app = express();
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 3000;
 
-app.use(cors());
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(express.static('public'));
 
-// connect to MongoDB
-mongoose.connect('mongodb://localhost/onboarding', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+app.post('/onboarding', (req, res) => {
+  const { name, email, password } = req.body;
+  // Do something with the user data
+  res.send('Onboarding successful!');
 });
 
-// define the user schema
-const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true },
-  password: { type: String, required: true },
-});
-
-// create the user model
-const User = mongoose.model('User', userSchema);
-
-// create a new user
-app.post('/api/users', async (req, res) => {
-  try {
-    const user = new User(req.body);
-    await user.save();
-    res.json({ success: true });
-  } catch (err) {
-    res.status(400).json({ error: err.message });
-  }
-});
-
-// start the server
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
 });
